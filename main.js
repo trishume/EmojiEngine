a854d714d8b37cb7200ed5fe7f8bf129d1975ec2
Tristan Hume <tris.hume@gmail.com>
2017-11-12 17:04:21 -0500

    Mouse control

--- main.js
+++ main.js
@@ -11,10 +11,14 @@ const textures = twgl.createTextures(gl, {
   }
 });
 
-const numItems = 50000;
+const numItems = 90000;
 const emojiDim = 49;
 const state = {
   aspect: 1.0,
+  mouseX: 0,
+  mouseY: 0,
+  altKey: false,
+  mouseButtons: 0,
   velocity: twgl.primitives.createAugmentedTypedArray(2, numItems),
 }
 const arrays = {
@@ -32,7 +36,15 @@ const uniforms = {
   u_emojiScale: window.devicePixelRatio,
 };
 
-function initialize(state, arrays) {
+// Credit to https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
+function randn_bm() {
+    var u = 0, v = 0;
+    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while(v === 0) v = Math.random();
+    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+}
+
+function initialize_data(state, arrays) {
   const velocity = state.velocity;
   const position = arrays.position;
   for (let i = 0; i < numItems*3; i += 3) {
@@ -47,13 +59,14 @@ function initialize(state, arrays) {
   for (let i = 0; i < numItems*3; i += 3) {
     info[i] = rand(0,emojiDim);
     info[i+1] = rand(0,emojiDim);
-    info[i+2] = rand(8.0,16.0);
+    info[i+2] = Math.max(8.0,randn_bm()*4.0 + 12.0);
   }
   console.log(info)
 }
 
 function update(state, arrays) {
-  const gravity = 0.001;
+  const gravity = 0.0001;
+  const friction = 0.99;
   const velocity = state.velocity;
   const position = arrays.position;
   for (let i = 0; i < numItems*3; i += 3) {
@@ -61,18 +74,38 @@ function update(state, arrays) {
     position[i+1] += velocity[i+1];
 
     // velocity[i+1] -= gravity;
+    velocity[i] *= friction;
+    velocity[i+1] *= friction;
+  }
 
-    if(position[i] < 0) {
-      velocity[i] = Math.abs(velocity[i]);
+  if(state.mouseButtons > 0) {
+    var factor = 0.0005;
+    if(state.altKey) {
+      factor *= -1;
     }
-    if(position[i] > state.aspect) {
-      velocity[i] = -Math.abs(velocity[i]);
+    for (let i = 0; i < numItems*3; i += 3) {
+      const dx = state.mouseX - position[i];
+      const dy = state.mouseY - position[i+1];
+      const dist2 = dx*dx + dy*dy;
+      velocity[i] += factor*(dx/dist2)
+      velocity[i+1] += factor*(dy/dist2)
     }
-    if(position[i+1] < 0) {
-      velocity[i+1] = Math.abs(velocity[i+1]);
-    }
-    if(position[i+1] > 1.0) {
-      velocity[i+1] = -Math.abs(velocity[i+1]);
+  }
+
+  if(state.mouseButtons == 0 || state.altKey) {
+    for (let i = 0; i < numItems*3; i += 3) {
+      if(position[i] < 0) {
+        velocity[i] = Math.abs(velocity[i]);
+      }
+      if(position[i] > state.aspect) {
+        velocity[i] = -Math.abs(velocity[i]);
+      }
+      if(position[i+1] < 0) {
+        velocity[i+1] = Math.abs(velocity[i+1]);
+      }
+      if(position[i+1] > 1.0) {
+        velocity[i+1] = -Math.abs(velocity[i+1]);
+      }
     }
   }
 }
@@ -108,5 +141,21 @@ function render(time) {
   requestAnimationFrame(render);
 }
 
-initialize(state, arrays);
-requestAnimationFrame(render);
+function mouse(event) {
+  const scale = gl.canvas.height / window.devicePixelRatio;
+  state.mouseX = event.clientX / scale;
+  state.mouseY = 1 - (event.clientY / scale);
+  state.mouseButtons = event.buttons;
+  state.altKey = event.altKey;
+}
+
+function init() {
+  initialize_data(state, arrays);
+  requestAnimationFrame(render);
+
+  document.addEventListener('mousemove', mouse);
+  document.addEventListener('mousedown', mouse);
+  document.addEventListener('mouseup', mouse);
+}
+
+window.onload = init;
